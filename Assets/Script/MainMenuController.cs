62758d5621506c26f6bd2e9bfc623d653c7dd90c
MineManiac <matheusbarrosr@gmail.com>
2024-11-03 22:49:07 -0300

    adicionado fase greenzone

--- Assets/Script/MainMenuController.cs
+++ Assets/Script/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void StartGame()
     {
-        LoadLevel("Local Multiplayer");
+        LoadLevel("StartGame");
     }
 
     public void ExitGame()
